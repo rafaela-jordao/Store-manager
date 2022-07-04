4038fada1bf45954551caa5acc0629f8d6336bd7
Rafaela Jordão <rafa.jordao.07@hotmail.com>
2022-07-04 16:48:14 -0300

    REQUISITO 4: validações para produtos

--- routers/productRoute.js
+++ routers/productRoute.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 const productController = require('../controllers/productController');
+const validationProduct = require('../middlewares/validationProduct');
 
 const productRoute = Router();
 
 productRoute.get('/', productController.list);
 productRoute.get('/:id', productController.findById);
-productRoute.post('/', productController.createProduct);
+productRoute.post('/', validationProduct, productController.createProduct);
 
 module.exports = productRoute;
